6dfa0bbb46f076487a6918f15235e2a23555ef19
HMRteknik <emailgithubkamu@example.com>
2025-11-20 23:26:33 +0700

    UPDATE

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,7 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p
-            className={`${lusitana.className}text-xl text-gray-800 md:text-3xl md:leading-normal`}
-          >
+            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Selamat Datang !</strong> <br></br>Ini adalah situs web
             pembelajaran{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
