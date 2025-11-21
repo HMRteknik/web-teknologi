c49ab5b13722d6af9348669ba647c5c134ea8fda
HMRteknik <hamaramaulana@gmail.com>
2025-11-21 10:44:41 +0700

    update env

--- auth.ts
+++ auth.ts
@@ -1,7 +1,6 @@
-// auth.ts (di root project)
 import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
+import { authConfig } from './auth.config';
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import type { User } from '@/app/lib/definitions';
@@ -17,29 +16,22 @@ async function getUser(email: string): Promise<User | undefined> {
   }
 }
 
-export const { auth, signIn, signOut, handlers } = NextAuth({
+export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [
     Credentials({
-      name: 'credentials',
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" }
-      },
       async authorize(credentials) {
         const parsedCredentials = z
-          .object({ 
-            email: z.string().email(), 
-            password: z.string().min(6) 
-          })
+          .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
 
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
           if (!user) return null;
+          
           const passwordsMatch = await bcrypt.compare(password, user.password);
-
+          
           if (passwordsMatch) return user;
         }
 
@@ -48,7 +40,4 @@ export const { auth, signIn, signOut, handlers } = NextAuth({
       },
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
-  trustHost: true,
-  useSecureCookies: process.env.NODE_ENV === 'production',
 });
